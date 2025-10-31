2f03fe4d238ce7be0e49512d1b3d3776f36da344
Matvei Newod <martnew07@mail.ru>
2025-10-31 15:31:52 +0300

    test_chief change

--- lab2/tests/management/test_chief_manager.py
+++ lab2/tests/management/test_chief_manager.py
@@ -11,6 +11,3 @@ def test_chief_manager_approve_budget():
     cm = ChiefManager("Главный")
     cm.approve_budget() == None
     
-def test_chief_manager_strategic_planning(capsys):
-    cm = ChiefManager("Главный")
-    assert cm.strategic_planning() == None
