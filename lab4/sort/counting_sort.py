57527eefeaf1994a96e2482f53d3a826e870ed45
Matvei Newod <martnew07@mail.ru>
2025-11-29 11:34:57 +0300

    feat: sort funcrtion is wrapped into a class

--- lab4/sort/counting_sort.py
+++ lab4/sort/counting_sort.py
@@ -1,23 +1,25 @@
-def counting_sort(non_sorted):
-    if len(non_sorted) <= 1:
-        return non_sorted
+class CountingSort():
+    @staticmethod
+    def counting_sort(non_sorted):
+        if len(non_sorted) <= 1:
+            return non_sorted
 
-    minn, maxx = min(non_sorted), max(non_sorted)
-    diff = maxx - minn + 1
+        minn, maxx = min(non_sorted), max(non_sorted)
+        diff = maxx - minn + 1
 
-    count = [0] * diff
+        count = [0] * diff
 
-    for num in non_sorted:
-        count[num - minn] += 1
+        for num in non_sorted:
+            count[num - minn] += 1
 
-    for i in range(1, len(count)):
-        count[i] += count[i - 1]
+        for i in range(1, len(count)):
+            count[i] += count[i - 1]
 
-    result = [0] * len(non_sorted)
+        result = [0] * len(non_sorted)
 
-    for num in reversed(non_sorted):
-        index = count[num - minn]
-        result[index - 1] = num
-        count[num - minn] -= 1
+        for num in reversed(non_sorted):
+            index = count[num - minn]
+            result[index - 1] = num
+            count[num - minn] -= 1
 
-    return result
+        return result
--- lab4/sort/tests/test_counting_sort.py
+++ lab4/sort/tests/test_counting_sort.py
@@ -1,16 +1,16 @@
 import pytest
 
-from lab4.sort.counting_sort import counting_sort
+from lab4.sort.counting_sort import CountingSort
 from lab4.sort.MyClass import Myclass
 
 
 def test_counting_sort_nums(non_sorted_nums, sorted_nums):
-    assert counting_sort(non_sorted_nums) == sorted_nums
+    assert CountingSort.counting_sort(non_sorted_nums) == sorted_nums
 
 
 def test_insertion_sort_empty():
-    assert counting_sort([]) == []
+    assert CountingSort.counting_sort([]) == []
 
 
 def test_counting_sort_single():
-    assert counting_sort([3]) == [3]
+    assert CountingSort.counting_sort([3]) == [3]
